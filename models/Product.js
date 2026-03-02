68b3a25132339e9353327d4c63fce4d2def8516d
usernameffas <151457809+usernameffas@users.noreply.github.com>
2026-03-03 01:36:35 +0900

    image not required

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,7 @@ const productSchema = Schema(
   {
     sku: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    image: { type: String, required: true },
+    image: { type: String, required: false },
     price: { type: Number, required: true },
     description: { type: String, required: true },
     stock: { type: Object, required: true }, // 예: { "S": 10, "M": 5 }
