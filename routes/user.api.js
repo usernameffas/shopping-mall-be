216ba78e5b208041be22654b0d807fc4823d71be
usernameffas <151457809+usernameffas@users.noreply.github.com>
2026-03-03 02:00:58 +0900

    1

--- routes/user.api.js
+++ routes/user.api.js
@@ -11,4 +11,4 @@ router.post("/login", userController.loginWithEmail);
 
 // 3. 내 정보 조회 (📍 문지기가 신분증 검사 후 통과시켜야 함)
 router.get("/me", authMiddleware.authenticate, userController.getUser);
-module.exports = router;
+module.exports = router; 
