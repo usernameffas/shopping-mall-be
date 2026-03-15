25cf1d45ba88f5d18c13e005145608b1c87f577e
usernameffas <151457809+usernameffas@users.noreply.github.com>
2026-03-15 21:06:55 +0900

    error fix

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -36,6 +36,19 @@ orderController.createOrder = async (req, res) => {
     cart.items = [];
     await cart.save();
 
+        // order 저장 후 stock 차감 추가
+    await order.save();
+
+    // 각 아이템 재고 차감
+    for (const item of cart.items) {
+      await Product.findByIdAndUpdate(item.productId._id, {
+        $inc: { [`stock.${item.size}`]: -item.qty }
+      });
+    }
+
+cart.items = [];
+await cart.save();
+
     res.status(200).json({ status: "success", orderNum });
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
