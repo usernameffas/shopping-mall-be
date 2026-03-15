87ff84f13c6c19149cac9899b848a972f1f2db07
usernameffas <151457809+usernameffas@users.noreply.github.com>
2026-03-15 09:33:42 +0900

    fix app.js syntax

--- app.js
+++ app.js
@@ -24,4 +24,4 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 서버가 ${PORT}번 포트에서 힘차게 돌아가는 중!`);
+  console.log(`🚀 서버가 ${PORT}번 포트에서 힘차게 돌아가는 중!`);});
